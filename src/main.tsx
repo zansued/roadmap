054a934c35fd3f50b743be2d40a6e94aa41a2963
Guilherme Zanini de Sá <zan.sued@gmail.com>
2026-03-06 17:54:16 -0300

    feat: initialize main file for React application

---
+++ src/main.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './index.css';
+
+declare global {
+  interface Window {
+    // Adicione quaisquer propriedades globais se necessário
+  }
+}
+
+const renderApp = (): void => {
+  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>
+  );
+};
+
+renderApp();
